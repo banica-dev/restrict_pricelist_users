691d3c8e9e0fd82eaf0f26787fb51d9beb89b5e5
danielsostenia <71837522+danielsostenia@users.noreply.github.com>
2026-01-03 13:30:08 +0200

    Remove price and currency from manifest
    Removed price and currency fields from manifest.

--- restrict_pricelist_users/__manifest__.py
+++ restrict_pricelist_users/__manifest__.py
@@ -7,8 +7,6 @@
     'author': 'Banica Daniel',
     'maintainers': ["banica-dev"],
     'license': 'OPL-1',
-    'price': 10,
-    'currency': "EUR",
     'installable': True,
     'data': [
         'views/product_pricelist_views.xml',
