b325a7e92b1d4dfc6c339751b6361fb8443faba2
Daniel <daniel.banica@sampamind.com>
2026-01-03 14:55:15 +0200

    update description

--- restrict_pricelist_users/__manifest__.py
+++ restrict_pricelist_users/__manifest__.py
@@ -3,7 +3,7 @@
     'name': 'Restrict Pricelist Users',
     'description': "This module restricts user access to specific pricelists in Odoo, "
                    "ensuring that only authorized users can view or manage pricelist data.",
-    'version': '16.0.1',
+    'version': '16.0.0.1',
     'author': 'Banica Daniel',
     'maintainers': ["banica-dev"],
     'license': 'OPL-1',
@@ -12,7 +12,11 @@
         'views/product_pricelist_views.xml',
         'security/pricelist_rules.xml',
     ],
-    'images': ['static/description/banner.png'],
+    'images': [
+        'static/description/banner.png',
+        'static/description/pricelist_allowed_users.png',
+        'static/description/user_groups.png',
+    ],
     'category': 'Sale',
     'depends': ['base', 'sale'],
 }
