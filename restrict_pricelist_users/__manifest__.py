e3a01147f64b34fb6fcef9d4d0c0bf9681690f2c
danielsostenia <daniel.banica1997@yahoo.com>
2026-01-05 13:15:47 +0200

    [MIG][19.0]

--- restrict_pricelist_users/__manifest__.py
+++ restrict_pricelist_users/__manifest__.py
@@ -3,7 +3,7 @@
     'name': 'Restrict Pricelist Users',
     'description': "This module restricts user access to specific pricelists in Odoo, "
                    "ensuring that only authorized users can view or manage pricelist data.",
-    'version': '18.0.0.1',
+    'version': '19.0.0.1',
     'author': 'Banica Daniel',
     'maintainers': ["banica-dev"],
     'license': 'OPL-1',
